1c12e5fb47fae47d50e62f1d76934a5803491af0
Brenda Fundesplai <brenda.fernandez@fundesplai.org>
2023-08-01 11:22:54 +0200

    Update __manifest__.py

--- create-form/__manifest__.py
+++ create-form/__manifest__.py
@@ -1,29 +1,18 @@
 {
-    "name": "Expenses Employees",
-    "description": "Create and check book checkout requests.",
+    "name": "New Form",
+    "description": "Create a form tu save data for portal users",
     "author": "Brenda Fernández",
-    "license": "AGPL-3",
     "depends": [
-        "hr_expense",
         "portal",
         "website",
     ],
     "data": [
-        "security/ir.model.access.csv",
-        "security/expense_security.xml",
-        "views/expense_create.xml",
-        "views/expense_info.xml",
-        "views/expense_list.xml",
-        "views/report_list.xml",
-        "views/report_info.xml",
-        "views/report_create.xml",
+        # "security/ir.model.access.csv",
+        # "security/expense_security.xml",
+        "views/form_template.xml",
     ],
     "assets": {
         "web.assets_common": [
-            "/expenses_portal/static/src/css/expense_list.css",
-            "/expenses_portal/static/src/css/report_list.css",
-            "/expenses_portal/static/src/css/report_info.css",
-            "/expenses_portal/static/src/css/expense.css",
             "web/static/lib/jquery/jquery.js",
         ]
     },
